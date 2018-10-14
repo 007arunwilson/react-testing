914ec3bf556be3d18b5589cb3d667fe29ff14b22
cyberinfoscripter <007arunwilson@gmail.com>
2018-10-14 21:34:48 +0530

    Adds test for app component

---
+++ src/__tests__/higherLevel.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from "../App";
+import Enzyme from 'enzyme';
+import { expect } from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+
+
+describe("App component", () => {
+  it("renders div with class App", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(".Apps")).to.have.lengthOf(1);
+  });
+});
